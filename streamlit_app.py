7ce377efc50e3c352a0177212da089ddd2df2643
TenThousandLakes <39275020+TenThousandLakes@users.noreply.github.com>
2022-06-06 17:14:54 -0500

    Update streamlit_app.py
    Found mistake.

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,13 +38,11 @@ try:
   if not fruit_choice:
      streamlit.error("Please select a fruit to get information.")
   else:
-     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-     streamlit.dataframe(fruityvice_normalized)
-      
+     back_from_function = get_fruityvice_data(fruit_choice)
+     streamlit.dataframe(back_from_function)
+
 except URLError as e:
   streamlit.error()
-  
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
@@ -66,6 +64,7 @@ def insert_row_snowflake(new_fruit):
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
                                                            
